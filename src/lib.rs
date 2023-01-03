a0a9cff1e3237b6130cc17b2a56c3ea579244655
Nicolás Castellán <cnicolas.developer@gmail.com>
2023-01-03 13:02:49 -0300

    🚧 Handle files not stdin and stdout and other basics
    The "reference" implementation of xml-rs handled io streams instead of
    files. So it was made to handle files.
    
    Additionally, the general structure of the program is basically
    compelete, so now it should be a matter of detecting the relevant
    information and calling `write_event`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,121 @@
-extern crate xml;
-
 use std::{
     error::Error,
     fs::File,
-    io::{self, BufReader, Write},
+    io::{BufReader, Write},
     path::PathBuf,
     result,
 };
 
-use xml::writer::{EmitterConfig, EventWriter, Result, XmlEvent};
+use xml::{
+    reader::EventReader,
+    writer::{EmitterConfig, EventWriter, Result},
+};
 
-pub fn write_event<W: Write>(w: &mut EventWriter<W>, line: String) -> Result<()> {
-    let line = line.trim();
+// TODO Idea: Use `output: Option<PathBuf>` to handle writing to a file or stdout.
+pub fn transform(input: PathBuf, output: PathBuf) -> result::Result<(), Box<dyn Error>> {
+    use xml::reader::XmlEvent;
 
-    let event: XmlEvent = if line.starts_with("+") && line.len() > 1 {
-        let line: Vec<&str> = line.split(' ').collect();
+    // Create the reader object.
+    let input_file = File::open(input)?;
+    let input_file = BufReader::new(input_file);
+    let parser = EventReader::new(input_file);
 
-        let name = line[0].get(1..).unwrap();
+    // Create the writer object.
+    let mut output_file = File::create(output)?;
+    let mut writer = EmitterConfig::new()
+        .perform_indent(true)
+        .indent_string("\t")
+        .create_writer(&mut output_file);
 
-        let mut event = XmlEvent::start_element(name);
+    {
+        // Use this sub-scope to process the xml.
+        for element in parser {
+            match element {
+                Ok(XmlEvent::Characters(line)) => {
+                    write_event(&mut writer, EventType::Content, &line)?;
+                }
+                // TODO: Determine if handling attributes is actually necessary.
+                Ok(XmlEvent::StartElement { name, .. }) => {
+                    let name = name.to_string();
+                    let name = simplify_name(&name);
 
-        for code in line {
-            let elements = code
-                .split('=')
-                .zip(code.split('=').skip(1))
-                .collect::<Vec<_>>();
+                    write_event(&mut writer, EventType::OpeningElement, name)?;
+                }
+                Ok(XmlEvent::EndElement { name }) => {
+                    let name = name.to_string();
+                    let name = simplify_name(&name);
 
-            for attributes in elements {
-                let (name, value) = attributes;
-                event = event.attr(name, value);
+                    write_event(&mut writer, EventType::ClosingElement, name)?;
+                }
+                Err(e) => {
+                    // TODO: Determine if there's a better way to handle this error.
+                    eprintln!("Error: {}", e);
+                    break;
+                }
+                _ => {}
             }
         }
+    }
 
-        event.into()
-    } else if line.starts_with("-") {
-        XmlEvent::end_element().into()
-    } else {
-        XmlEvent::characters(&line).into()
+    Ok(())
+}
+
+/// Internal enum to help [`transform`](transform) communicate instructions to
+/// [`write_event`](write_event).
+enum EventType {
+    OpeningElement,
+    ClosingElement,
+    Content,
+}
+
+fn write_event<W: Write>(w: &mut EventWriter<W>, event_type: EventType, line: &str) -> Result<()> {
+    use xml::writer::XmlEvent;
+
+    let line = line.trim();
+
+    let event: XmlEvent = match event_type {
+        EventType::OpeningElement => {
+            let line: Vec<&str> = line.split(' ').collect();
+
+            let name = line[0].get(1..).unwrap();
+
+            let mut event = XmlEvent::start_element(name);
+
+            for code in line {
+                let elements = code
+                    .split('=')
+                    .zip(code.split('=').skip(1))
+                    .collect::<Vec<_>>();
+
+                for attributes in elements {
+                    let (name, value) = attributes;
+                    event = event.attr(name, value);
+                }
+            }
+
+            event.into()
+        }
+        EventType::ClosingElement => XmlEvent::end_element().into(),
+        EventType::Content => XmlEvent::characters(&line).into(),
     };
 
     w.write(event)
 }
 
-pub fn transform(input: PathBuf, output: PathBuf) -> result::Result<(), Box<dyn Error>> {
-    // Open and create files
-    let input_file = File::open(input)?;
-    let mut output_file = File::create(output)?;
+/// Takes a string that would look something like
+///
+/// `{http:://www.opengis.net/kml/2.2}coordinates`
+///
+/// and removes the link by splitting the &str at the '}' and returning the element to the right.
+///
+/// `coordinates`
+fn simplify_name<'a>(name: &'a str) -> &'a str {
+    let is_split = match name.find('}') {
+        Some(_) => 1,
+        None => 0,
+    };
 
-    // Create the io objects
-    let input = io::stdin();
-    let mut output = io::stdout();
-    let mut writer = EmitterConfig::new()
-        .perform_indent(true)
-        .create_writer(&mut output_file);
+    let split_name: Vec<&str> = name.split('}').collect();
 
-    // Supposed to process the .kml file's data to make a useful .fgfp, for now it just processes
-    // user input.
-    loop {
-        print!("> ");
-        output.flush().unwrap();
-        let mut line = String::new();
-        match input.read_line(&mut line) {
-            Ok(0) => break,
-            Ok(_) => match write_event(&mut writer, line) {
-                Ok(_) => {}
-                Err(e) => panic!("Write error: {}", e),
-            },
-            Err(e) => panic!("Input error: {}", e),
-        }
-    }
-
-    Ok(())
+    split_name[is_split]
 }
