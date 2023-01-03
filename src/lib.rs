2da81c826763cefbaf86e81561c4dfd1c72dc6f9
Nicolás Castellán <cnicolas.developer@gmail.com>
2023-01-03 14:05:43 -0300

    🐎 Applied a few tweaks from clippy
    And corrected a documentation comment referencing a String when it
    should reference &str.

--- src/lib.rs
+++ src/lib.rs
@@ -12,27 +12,27 @@ use xml::writer::{EventWriter, Result};
 // TODO Idea: Use `output: Option<PathBuf>` to handle writing to a file or stdout.
 pub fn transform_route<W: Write, R: Read>(
     parser: EventReader<R>,
-    mut writer: &mut EventWriter<W>,
+    writer: &mut EventWriter<W>,
 ) -> result::Result<(), Box<dyn Error>> {
     use xml::reader::XmlEvent;
 
     for element in parser {
         match element {
             Ok(XmlEvent::Characters(line)) => {
-                write_event(&mut writer, EventType::Content, &line)?;
+                write_event(writer, EventType::Content, &line)?;
             }
             // TODO: Determine if handling attributes is actually necessary.
             Ok(XmlEvent::StartElement { name, .. }) => {
                 let name = name.to_string();
                 let name = simplify_name(&name);
 
-                write_event(&mut writer, EventType::OpeningElement, name)?;
+                write_event(writer, EventType::OpeningElement, name)?;
             }
             Ok(XmlEvent::EndElement { name }) => {
                 let name = name.to_string();
                 let name = simplify_name(&name);
 
-                write_event(&mut writer, EventType::ClosingElement, name)?;
+                write_event(writer, EventType::ClosingElement, name)?;
             }
             Err(e) => {
                 // TODO: Determine if there's a better way to handle this error.
@@ -82,7 +82,7 @@ fn write_event<W: Write>(w: &mut EventWriter<W>, event_type: EventType, line: &s
             event.into()
         }
         EventType::ClosingElement => XmlEvent::end_element().into(),
-        EventType::Content => XmlEvent::characters(&line).into(),
+        EventType::Content => XmlEvent::characters(line),
     };
 
     w.write(event)
@@ -91,36 +91,36 @@ fn write_event<W: Write>(w: &mut EventWriter<W>, event_type: EventType, line: &s
 /// Write the start of the .fgfp's xml tree. AKA the version, flight-rules, flight-type and
 /// estimated duration.
 #[rustfmt::skip]
-pub fn write_start_of_tree<W: Write>(mut writer: &mut EventWriter<W>) -> Result<()> {
-    write_event(&mut writer, EventType::OpeningElement, "PropertyList")?;
+pub fn write_start_of_tree<W: Write>(writer: &mut EventWriter<W>) -> Result<()> {
+    write_event(writer, EventType::OpeningElement, "PropertyList")?;
 
-    write_event(&mut writer, EventType::OpeningElement, "version type=int")?;
-    write_event(&mut writer, EventType::Content, "2")?;
-    write_event(&mut writer, EventType::ClosingElement, "version")?;
+    write_event(writer, EventType::OpeningElement, "version type=int")?;
+    write_event(writer, EventType::Content, "2")?;
+    write_event(writer, EventType::ClosingElement, "version")?;
 
-    write_event(&mut writer, EventType::OpeningElement, "flight-rules type=string")?;
-    write_event(&mut writer, EventType::Content, "V")?;
-    write_event(&mut writer, EventType::ClosingElement, "flight-rules")?;
+    write_event(writer, EventType::OpeningElement, "flight-rules type=string")?;
+    write_event(writer, EventType::Content, "V")?;
+    write_event(writer, EventType::ClosingElement, "flight-rules")?;
 
-    write_event(&mut writer, EventType::OpeningElement, "flight-type type=string")?;
-    write_event(&mut writer, EventType::Content, "X")?;
-    write_event(&mut writer, EventType::ClosingElement, "flight-type")?;
+    write_event(writer, EventType::OpeningElement, "flight-type type=string")?;
+    write_event(writer, EventType::Content, "X")?;
+    write_event(writer, EventType::ClosingElement, "flight-type")?;
 
-    write_event(&mut writer, EventType::OpeningElement, "estimated-duration-minutes type=int")?;
-    write_event(&mut writer, EventType::Content, "0")?;
-    write_event(&mut writer, EventType::ClosingElement, "estimated-duration-minutes")?;
+    write_event(writer, EventType::OpeningElement, "estimated-duration-minutes type=int")?;
+    write_event(writer, EventType::Content, "0")?;
+    write_event(writer, EventType::ClosingElement, "estimated-duration-minutes")?;
 
     Ok(())
 }
 
-/// Takes a string that would look something like
+/// Takes a [`&str`](str) that would look something like
 ///
 /// `{http:://www.opengis.net/kml/2.2}coordinates`
 ///
 /// and removes the link by splitting the &str at the '}' and returning the element to the right.
 ///
 /// `coordinates`
-fn simplify_name<'a>(name: &'a str) -> &'a str {
+fn simplify_name(name: &str) -> &str {
     let is_split = match name.find('}') {
         Some(_) => 1,
         None => 0,
